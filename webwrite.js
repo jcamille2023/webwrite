c7d60cf12f73460fb8ddcbfff6fb8c90127c0209
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2024-03-18 16:29:28 -0400

    Enhancing WebWrite

--- webwrite.js
+++ webwrite.js
@@ -12,14 +12,17 @@ function WebWrite() {
   // creating container that will hold all text
   let text_container = document.createElement('div');
   container.appendChild(text_container);
-  
-  
-  
   let h1_button = document.createElement("button");
   h1_button.appendChild(document.createTextNode('Heading'));
   h1_button.addEventListener('click', () => {
     let h1_text = document.createElement('h1');
-    h1_text.contenteditable = 'true';
+    h1_text.addEventListener('keydown', (e) => {
+      if(e.key == 'Backspace' && p_text.innerHTML == '') {
+        h1_text.remove();
+      }
+    });
+    h1_text.appendChild(document.createTextNode('New heading'));
+    h1_text.setAttribute('contenteditable', 'true');
     text_container.appendChild(h1_text);
   });
   option_buttons.appendChild(h1_button);
@@ -27,10 +30,16 @@ function WebWrite() {
   p_button.appendChild(document.createTextNode('Paragraph'));
   p_button.addEventListener('click', () => {
     let p_text = document.createElement('p');
-    p_text.contenteditable = 'true';
+    p_text.addEventListener('keydown', (e) => {
+      if(e.key == 'Backspace' && p_text.innerHTML == '') {
+        p_text.remove();
+      }
+    });
+    p_text.appendChild(document.createTextNode('New paragraph'));
+    p_text.setAttribute('contenteditable', 'true');
     text_container.appendChild(p_text);
   });
   option_buttons.appendChild(p_button);
 
-  return container;
+  return [container, text_container];
 }
